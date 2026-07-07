fc231c480f838bb65acd3165f4a48e826e1e5236
GabryelHenrique <gabryeleducaufu@gmail.com>
2026-07-07 09:32:08 -0300

    feat: cadastro de transacoes

--- backend/ControleGastos.API/Controllers/TransacoesController.cs
+++ backend/ControleGastos.API/Controllers/TransacoesController.cs
@@ -1,5 +1,11 @@
 using ControleGastos.API.Data;
 using Microsoft.AspNetCore.Mvc;
+using ControleGastos.API.Models;
+using Microsoft.EntityFrameworkCore;
+using ControleGastos.API.Dtos;
+using ControleGastos.API.Enums;
+
+
 
 namespace ControleGastos.API.Controllers;
 
@@ -14,4 +20,65 @@ public class TransacoesController : ControllerBase
         _context = context;
     
     }
+
+[HttpGet]
+public async Task<ActionResult<List<Transacao>>> ListarTransacoes()
+{
+    var transacoes = await _context.Transacoes.ToListAsync();
+
+    return Ok(transacoes);
+}
+
+[HttpGet("{id}")]
+public async Task<ActionResult<Transacao>> BuscarTransacaoPorId(int id)
+    {
+        var transacao = await _context.Transacoes.FindAsync(id);
+
+        if (transacao == null)
+        {
+            return NotFound("Transação não encontrada.");
+        }
+        return Ok(transacao);
+    }
+
+[HttpPost]
+public async Task<ActionResult<Transacao>> CadastrarTransacao(CriarTransacaoDto transacaoDto)
+{
+    if (string.IsNullOrWhiteSpace(transacaoDto.Descricao))
+    {
+        return BadRequest("A descrição da transação é obrigatória.");
+    
+    }
+    
+    if (transacaoDto.Valor <= 0)
+    {
+        return BadRequest("O valor da transação deve ser maior que zero.");   
+    }
+
+    var pessoa = await _context.Pessoas.FindAsync(transacaoDto.PessoaId);
+
+    if (pessoa == null)
+    {
+        return BadRequest("A pessoa informada não existe.");   
+    } 
+
+    if (pessoa.Idade < 18 && transacaoDto.Tipo == TipoTransacao.Receita)
+    {
+        return BadRequest("Pessoas menores de idade podem cadastrar somente despesas."); 
+    }  
+
+    var transacao = new Transacao
+    {
+        Descricao = transacaoDto.Descricao,
+        Valor = transacaoDto.Valor,
+        Tipo = transacaoDto.Tipo,
+        PessoaId = transacaoDto.PessoaId
+    };
+
+    _context.Transacoes.Add(transacao);
+    await _context.SaveChangesAsync();
+
+    return CreatedAtAction(nameof(BuscarTransacaoPorId), new { id = transacao.Id }, transacao);
+}
+
 }
